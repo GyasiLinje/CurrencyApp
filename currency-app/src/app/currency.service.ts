5604ea342326d386269d72e81a90d1f8b50e083f
Gyasi11 <glcalhoun1123@yahoo.com>
2020-04-08 01:30:06 -0400

    Structure

---
+++ currency-app/src/app/currency.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { from } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+import { Rate } from './rate';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class CurrencyService {
+
+  //constructor(private http: HttpClient) { }
+
+
+}
--- currency-app/src/app/home/home.component.ts
+++ currency-app/src/app/home/home.component.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map, catchError, tap } from 'rxjs/operators';
+import { Rate } from '../rate';
+import { CurrencyService } from '../currency.service'
 
 @Component({
   selector: 'app-home',
@@ -10,21 +12,18 @@ import { map, catchError, tap } from 'rxjs/operators';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  
+
   endpoint = 'http://data.fixer.io/latest?access_key=a09ee58652dcda275398016195c7b149';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private rates: Rate) { }
 
-  private extractData(res: Response) {
-    let body = res;
-    return body || {};
-  }
 
   ngOnInit(): void {
     this.getData();
   }
 
   getData() {
+
     console.log('Hello')
     return this.http.get(this.endpoint).subscribe(
       data => {
@@ -33,7 +32,7 @@ export class HomeComponent implements OnInit {
     )
   }
 
-  
+ 
 
 
 }
