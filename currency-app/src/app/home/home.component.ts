cf1d405224e041a60a050426a245ed860a37b85a
Gyasi11 <glcalhoun1123@yahoo.com>
2020-04-08 01:00:34 -0400

    Add routing and http

---
+++ currency-app/src/app/home/home.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  
+  endpoint = 'http://data.fixer.io/latest?access_key=a09ee58652dcda275398016195c7b149';
+
+  constructor(private http: HttpClient) { }
+
+  private extractData(res: Response) {
+    let body = res;
+    return body || {};
+  }
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData() {
+    console.log('Hello')
+    return this.http.get(this.endpoint).subscribe(
+      data => {
+        console.log(data)
+      }
+    )
+  }
+
+  
+
+
+}
